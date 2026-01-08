3e8db3a1c6918a22792223998a3defa33cec4ff3
chadireoroonu <chadireoroonu@gmail.com>
2026-01-08 14:01:28 +0900

    2601084 refactoring

--- 01/P08_p_42587.py
+++ 01/P08_p_42587.py
@@ -5,19 +5,23 @@
 from collections import deque
 
 def solution(priorities, location):
-    answer = 0
+    answer = 0 # 현재까지 실행된 프로세스 수
     queue = deque()
     
     for i in enumerate(priorities):
         queue.append(i)
     
+    # 우선순위 내림차순 정렬
+    priorities.sort(reverse=True)
+    
     while queue:
-        maxi = max(queue, key=lambda x:x[1])[1]
         x, y = queue.popleft()
-        if y < maxi:
+        # 남은 프로세스 중 현재 프로세스가 가장 우선순위가 높지 않은 경우
+        if y < priorities[answer]:
             queue.append((x, y))
-        else:
+        else: # 현재 프로세스 우선순위가 가장 높은 경우
             answer += 1
+            # 현재 프로세스가 순서가 궁금한 프로세스일 경우
             if x == location:
                 break
     
