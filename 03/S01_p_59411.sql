f6c06adfc55062a23a9a237c4c5f4affbbf72da8
chadireoroonu <chadireoroonu@gmail.com>
2026-03-01 15:05:44 +0900

    2603017 sql

---
+++ 03/S01_p_59411.sql
@@ -0,0 +1,8 @@
+-- 오랜 기간 보호한 동물(2)
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59411
+
+SELECT I.ANIMAL_ID, I.NAME
+FROM ANIMAL_INS AS I
+JOIN ANIMAL_OUTS AS O
+    ON I.ANIMAL_ID = O.ANIMAL_ID
+ORDER BY O.DATETIME - I.DATETIME DESC LIMIT 2
