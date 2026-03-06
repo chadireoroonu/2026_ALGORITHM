9c6cff759f18d3b05eece64abde52a0e6cf94cb0
chadireoroonu <chadireoroonu@gmail.com>
2026-03-06 15:39:46 +0900

    2603065 sql

---
+++ 03/S06_p_132204.sql
@@ -0,0 +1,13 @@
+-- 취소되지 않은 진료 예약 조회하기
+-- https://school.programmers.co.kr/learn/courses/30/lessons/132204
+
+SELECT A.APNT_NO, P.PT_NAME, A.PT_NO, A.MCDP_CD, D.DR_NAME, A.APNT_YMD
+FROM APPOINTMENT AS A
+JOIN DOCTOR AS D
+    ON A.MDDR_ID = D.DR_ID
+JOIN PATIENT AS P
+    ON A.PT_NO = P.PT_NO
+WHERE A.APNT_CNCL_YMD IS NULL
+    AND A.MCDP_CD = 'CS'
+    AND APNT_YMD LIKE '2022-04-13%'
+ORDER BY APNT_YMD ASC
