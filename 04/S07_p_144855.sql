184362d9767ba9b33c0014c88ee28a334e664e15
chadireoroonu <chadireoroonu@gmail.com>
2026-04-07 16:08:46 +0900

    2604072 sql

---
+++ 04/S07_p_144855.sql
@@ -0,0 +1,9 @@
+-- 카테고리 별 도서 판매량 집계하기
+-- https://school.programmers.co.kr/learn/courses/30/lessons/144855
+
+SELECT B.CATEGORY, SUM(S.SALES) AS TOTAL_SALES
+FROM BOOK AS B
+JOIN BOOK_SALES AS S ON B.BOOK_ID = S.BOOK_ID
+WHERE SALES_DATE LIKE '2022-01%'
+GROUP BY CATEGORY
+ORDER BY CATEGORY
